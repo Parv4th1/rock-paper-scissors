4a9b9c2d8d0e3796f190abcba34c492098cdf6ea
Parv4th1 <parvathi.mail09@gmail.com>
2024-03-01 11:48:09 +0530

    Added playRound()

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -13,3 +13,36 @@ function getComputerChoice()
             return 'Something wrong with comper choice'
     }
 }
+function playRound(playerSelection, computerSelection)
+{
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection == computerSelection)
+    {
+        return 'Tie!'
+    }
+    else if(playerSelection == 'rock')
+    {
+        if(computerSelection == 'scissors')
+            return 'You win! Rock beats Scissors'
+        else
+            return 'You lose! Paper beats Rock'
+    }
+    else if(playerSelection == 'scissors')
+    {
+        if(computerSelection == 'paper')
+            return 'You win! Scissors beats Paper'
+        else
+            return 'You lose! Rock beats Scissors'
+    }
+    else if(playerSelection == 'paper')
+    {
+        if(computerSelection == 'rock')
+            return 'You win! Paper beats Rock'
+        else
+            return 'You lose! Scissors beats Paper'
+    }
+}
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
